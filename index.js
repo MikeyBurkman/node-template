27a05c8748e39d1b8246a9a9b949530073c8d1bf
MikeyBurkman <michael.burkman@gmail.com>
2017-05-30 09:36:21 -0400

    Merge json properties

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 const path = require('path');
 const fs = require('fs-extra');
+const Promise = require('bluebird');
+const isObject = require('is-plain-object');
 
 const destDir = process.cwd();
 const templateDir = path.resolve(__dirname, 'template');
@@ -44,12 +46,33 @@ function jsonCopy(fname) {
     const dest = path.resolve(destDir, fname);
     return fs.exists(dest).then((exists) => {
         if (exists) {
-            // TODO: Do a shallow merge of JSON properites
-            console.log(`File ${fname} already exists, skipping it. (Merging for this file will soon be implemented)`);
-            return;
+            console.log(`File ${fname} already exists, merging new properties if necessary`);
+            return Promise.props({
+                base: fs.readFile(src),
+                updates: fs.readFile(dest)
+            }).then((o) => {
+                const newPj = mergeJson(JSON.parse(o.base), JSON.parse(o.updates));
+                return fs.writeFile(dest, JSON.stringify(newPj, null, 2));
+            });
         } else {
             console.log(`Copying file ${fname}`);
             return fs.copy(src, dest);
         }
     });
+}
+
+function mergeJson(baseJson, updatesJson) {
+    const res = {};
+    Object.keys(updatesJson).forEach((key) => {
+        if (baseJson[key] === undefined) {
+            res[key] = updatesJson[key];
+        } else if (isObject(baseJson[key])) {
+            // Merge the two objects, but don't overwrite anything!
+            res[key] = Object.assign({}, updatesJson[key], baseJson[key]);
+        } else {
+            // Already exists and isn't an object so we can't merge anything, so just ignore it
+            res[key] = baseJson[key];
+        }
+    });
+    return res;
 }
